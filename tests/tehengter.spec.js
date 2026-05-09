e4cbee15b8c7590fb277d8af3f04acc1665cd0ed
Szász Benedek <benedekszasz7@gmail.com>
2026-05-09 13:00:17 +0200

    3 teszt kész

--- tests/tehengter.spec.js
+++ tests/tehengter.spec.js
@@ -11,3 +11,20 @@
 import { test, expect } from '@playwright/test';
 
 const url = 'https://szit.hu/m/tehengter/'
+
+test('A címsor Henger', async ({ page }) => {
+    await page.goto(url)
+    await expect(page).toHaveTitle(/Henger/)
+})
+
+test('A h1 elemben Henger térfogata', async ({ page }) => {
+    await page.goto(url)
+    const text = page.locator('h1')
+    await expect(text).toContainText('Henger térfogata')
+})
+
+test('A p elem tartalma', async ({ page }) => {
+    await page.goto(url)
+    const text = page.locator('p')
+    await expect(text).toContainText('Henger térfogatának számítása az alap sugarából és a henger magasságából.')
+})
