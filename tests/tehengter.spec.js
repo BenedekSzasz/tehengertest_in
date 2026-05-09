19c0bb12f33ba2d4d4349ad3dcb8a1c0dcd7e37d
Szász Benedek <benedekszasz7@gmail.com>
2026-05-09 12:49:35 +0200

    kezdés

---
+++ tests/tehengter.spec.js
@@ -0,0 +1,13 @@
+/*
+* File: tehengter.spec.js
+* Author: Szász Benedek
+* Copyright: 2026, Szász Benedek
+* Group: Szoft I N
+* Date: 2026-05-08
+* Github: https://github.com/BenedekSzasz/
+* Licenc: MIT
+*/
+
+import { test, expect } from '@playwright/test';
+
+const url = 'https://szit.hu/m/tehengter/'
