214ee0caa822a1a6fadbd615376797d62a5261cc
Szász Benedek <benedekszasz7@gmail.com>
2026-05-09 14:24:25 +0200

    Mindegyik teszt átment

--- tests/tehengter.spec.js
+++ tests/tehengter.spec.js
@@ -38,47 +38,38 @@ test('input 30, 35 output:98960.16858807848', async ({ page }) => {
     await expect(volume).toHaveValue('98960.16858807848')
 })
 
-test('input 30, 35 output:98960.16858807848', async ({ page }) => {
+test('input 52, 54 output:458722.7929065672', async ({ page }) => {
     await page.goto(url)
-    await page.locator('#radius').fill('30')
-    await page.locator('#height').fill('35')
+    await page.locator('#radius').fill('52')
+    await page.locator('#height').fill('54')
     await page.locator('#calcButton').click()
     const volume = page.locator('#volume')
-    await expect(volume).toHaveValue('98960.16858807848')
+    await expect(volume).toHaveValue('458722.7929065672')
 })
 
-test('input 30, 35 output:98960.16858807848', async ({ page }) => {
+test('input 128, 114 output:5867791.360151329', async ({ page }) => {
     await page.goto(url)
-    await page.locator('#radius').fill('30')
-    await page.locator('#height').fill('35')
+    await page.locator('#radius').fill('128')
+    await page.locator('#height').fill('114')
     await page.locator('#calcButton').click()
     const volume = page.locator('#volume')
-    await expect(volume).toHaveValue('98960.16858807848')
+    await expect(volume).toHaveValue('5867791.360151329')
 })
 
-test('input 30, 35 output:98960.16858807848', async ({ page }) => {
+test('input 0, 35 output:0', async ({ page }) => {
     await page.goto(url)
-    await page.locator('#radius').fill('30')
+    await page.locator('#radius').fill('0')
     await page.locator('#height').fill('35')
     await page.locator('#calcButton').click()
     const volume = page.locator('#volume')
-    await expect(volume).toHaveValue('98960.16858807848')
+    await expect(volume).toHaveValue('0')
 })
 
-test('input 30, 35 output:98960.16858807848', async ({ page }) => {
+test('input 30, 0 output:0', async ({ page }) => {
     await page.goto(url)
     await page.locator('#radius').fill('30')
-    await page.locator('#height').fill('35')
+    await page.locator('#height').fill('0')
     await page.locator('#calcButton').click()
     const volume = page.locator('#volume')
-    await expect(volume).toHaveValue('98960.16858807848')
-})
-
-test('input 30, 35 output:98960.16858807848', async ({ page }) => {
-    await page.goto(url)
-    await page.locator('#radius').fill('30')
-    await page.locator('#height').fill('35')
-    await page.locator('#calcButton').click()
-    const volume = page.locator('#volume')
-    await expect(volume).toHaveValue('98960.16858807848')
+    await expect(volume).toHaveValue('0')
 })
